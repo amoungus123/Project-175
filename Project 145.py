165d4fdd4d840cedd3a2c507ae6d3f118f0d4e0c
amoungus123 <76024871+amoungus123@users.noreply.github.com>
2023-01-18 20:43:18 -0500

    Add files via upload

---
+++ Project 145.py
@@ -0,0 +1,18 @@
+import matplotlib .pyplot as plt
+import psutil as p
+app_name_list = []
+app_name_percentage_list = []
+count = 0
+for process in p.process_iter():
+    count = count +1
+    if count <=10:
+        name = process.name()
+        cpu_usage = p.cpu_percent()
+        print('name: ', name,'--- cpu_usage: ', cpu_usage)
+        app_name_list.append(name)
+        app_name_percentage_list.append(cpu_usage)
+plt.figure(figsize=(15,7))
+plt.xlabel("Min-Max CPU Consumption")
+plt.ylabel("Usage")
+plt.bar(app_name_list, app_name_percentage_list,width=0.6,color=("red", "orange", "yellow", "green", "darkblue", "blue", "purple", "pink", "black", "gray"))
+plt.show()
